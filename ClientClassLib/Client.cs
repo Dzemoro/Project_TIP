c6121046bae33c0dbfaa14c7e9cb06a1ee172e00
sulkowski99 <sulkowski99@gmail.com>
2021-03-24 20:00:43 +0100

    Repair commit. I'm sorry :(

---
+++ ClientClassLib/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ClientClassLib
+{
+    public class Client
+    {
+        public Client() { }
+        public void sendMessage(IPAddress address,String msg)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+           // IPAddress address = IPAddress.Parse("127.0.0.1");
+           
+            byte[] sendbuf = Encoding.ASCII.GetBytes(msg);
+            IPEndPoint ep = new IPEndPoint(address, 11000);
+            s.SendTo(sendbuf, ep);
+            Console.WriteLine("Message sent to the address");
+        }
+        
+
+    }
+}
