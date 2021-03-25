75488d23125a0c9d68ca33d18ea7633c3c221484
Dzemoro <dellorpl@gmail.com>
2021-03-25 15:24:08 +0100

    Simple server and added NAudio

--- Server/Server.cs
+++ Server/Server.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServerClassLib;
 
 namespace Server
 {
-    class Program
+    class Server
     {
         static void Main(string[] args)
         {
+            ServerUDP server = new ServerUDP("127.0.0.1", 11000);
+            server.Listener();
         }
     }
 }
